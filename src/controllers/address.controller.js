389c8816193500f4ea181a1ec2950498a394308f
TuanAnh19122003 <gamethunghiem19122003@gmail.com>
2025-12-15 13:39:03 +0700

    add

--- src/controllers/address.controller.js
+++ src/controllers/address.controller.js
@@ -1,20 +1,53 @@
 const { fetchProvinces, fetchDistricts, fetchWards } = require('../utils/apiProvinces');
 
 exports.getProvinces = async (req, res) => {
-    const data = await fetchProvinces();
-    res.json(data);
+    try {
+        const data = await fetchProvinces();
+        return res.json({
+            success: true,
+            data
+        });
+    } catch (error) {
+        console.error('Lỗi khi lấy danh sách tỉnh:', error);
+        return res.status(500).json({
+            success: false,
+            message: 'Không lấy được danh sách tỉnh'
+        });
+    }
 };
 
 exports.getDistricts = async (req, res) => {
-    const { provinceCode } = req.params;
-    const data = await fetchDistricts(provinceCode);
-    res.json(data);
+    try {
+        const { provinceCode } = req.params;
+        const data = await fetchDistricts(provinceCode);
+        return res.json({
+            success: true,
+            data
+        });
+    } catch (error) {
+        console.error('Lỗi khi lấy quận/huyện:', error);
+        return res.status(500).json({
+            success: false,
+            message: 'Không lấy được danh sách quận/huyện'
+        });
+    }
 };
 
 exports.getWards = async (req, res) => {
-    const { districtCode } = req.params;
-    const data = await fetchWards(districtCode);
-    res.json(data);
+    try {
+        const { districtCode } = req.params;
+        const data = await fetchWards(districtCode);
+        return res.json({
+            success: true,
+            data
+        });
+    } catch (error) {
+        console.error('Lỗi khi lấy phường/xã:', error);
+        return res.status(500).json({
+            success: false,
+            message: 'Không lấy được danh sách phường/xã'
+        });
+    }
 };
 
 exports.submitAddress = (req, res) => {
