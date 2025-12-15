a387a4067b22eaea7d4dc2db48a3a1a535a6adba
TuanAnh19122003 <gamethunghiem19122003@gmail.com>
2025-12-15 12:46:32 +0700

    fix database url

--- src/config/database.js
+++ src/config/database.js
@@ -1,23 +1,16 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASS,
-    {
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
-        dialect: 'postgres',
-        logging: false,
-        dialectOptions: {
-            ssl: {
-                require: true,
-                rejectUnauthorized: false,
-            },
-        },
-        timezone: '+07:00',
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres',
+    protocol: 'postgres',
+    logging: false,
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+        }
     }
-);
+});
 
 module.exports = sequelize;
